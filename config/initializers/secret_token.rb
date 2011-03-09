9657218e595f89ce130293b639661b6717436c06
Fig <github@thebeach.plus.com>
2011-03-09 00:41:23 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gib::Application.config.secret_token = '1d89a7f507531e0841e0bd2ba9065e34720b2e7d8657d5f5e5ca2eba9d11e849f664964afb9451da6f6b738843c064ffd916d8c0981db50589af4a8830291cc0'
